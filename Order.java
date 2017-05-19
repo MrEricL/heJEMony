067823b3c8001a594b74f52bcec48e675e101516
Jenny Han <jhan6@stuy.edu>
2017-05-19 10:09:40 -0400

    order

--- Order.java
+++ Order.java
@@ -1,9 +1,33 @@
 public class Order {
-    private ALQueue<Food> _order; 
+    private ALQueue<Food> _order;
 
-    public void addToOrder(){
+    public Order (int items) {
+	_order.enqueue (new Bun());
+	for (int i = 0; i < items; i ++ ) {
+	    _order.enqueue (randomFood());
+	}
+	_order.enqueue (new Bun());
     } 
 
-    public Food constructOrder(){
+    private static Food randomFood () {
+	int number = (int) (Math.random () * 4 + 1); 
+	if (number == 1) {
+	    return new Patty();
+	}
+	else if (number == 2) {
+	    return new Cheese();
+	}
+	else if (number == 3) {
+	    return new Lettuce();
+	}
+	else {
+	    return new Tomato();
+	}
     }
+
+    public String toString () {
+	String ret = _order.toString();
+	return ret.substring (7, ret.length() -5);
+    } 
+
 } 
