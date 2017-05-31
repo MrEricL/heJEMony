ded6d9b952ca49a5d95e5c80abeb4f114113f0e0
Matteo Wong <Matteo@Matteos-MacBook-Air.local>
2017-05-30 21:14:57 -0400

    flows

--- Bun.java
+++
@@ -1,8 +0,0 @@
-public class Bun extends Food {
-
-    public Bun() {
-	_name="Bun";
-	_num=0;
-    }
-
-}
--- Cheese.java
+++
@@ -1,6 +0,0 @@
-public class Cheese extends Food {
-    public Cheese() {
-	_name="cheese";
-	_num=2;
-    }
-}
